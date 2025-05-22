57b46d5a6b34979463f529afe3ec67ad5998af38
seung-yuppy <songsy0107@gmail.com>
2025-05-22 17:17:43 +0900

    Fix. 말풍선 높이 수정

--- components/speech.tsx
+++ components/speech.tsx
@@ -7,7 +7,7 @@ const Speech = ({ text, direction = "top" }: ISpeech) => {
   return (
     <div className="relative px-5">
       {direction !== 'bottom' && <div className="arrow-top" />}
-      <div className="bg-white border-2 border-black rounded-full p-6 w-64 text-black text-center absolute bottom-2" >
+      <div className="bg-white border-2 border-black rounded-full p-6 w-64 min-h-20 text-black text-center absolute bottom-2" >
         {text}
       </div>
       {direction === 'bottom' && <div className="arrow-bottom" />}
